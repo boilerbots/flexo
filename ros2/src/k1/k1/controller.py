c224572bd119b7343f046102e3bbc7cbd577a518
Curt Meyers <boilerbots@gmail.com>
2024-03-24 21:57:46 -0700

    Added CORRECTION and it just jerk on startup.

--- ros2/src/k1/k1/controller.py
+++ ros2/src/k1/k1/controller.py
@@ -10,7 +10,7 @@ import copy
 import rclpy
 from rclpy.node import Node
 from ament_index_python import packages
-from rclpy.callback_groups import ReentrantCallbackGroup
+from rclpy.callback_groups import MutuallyExclusiveCallbackGroup, ReentrantCallbackGroup
 from k1_interfaces.msg import Status, Joints
 from k1_interfaces.srv import JointControl
 
@@ -30,6 +30,7 @@ class Controller(Node):
     State = enum.Enum('State', ['IDLE', 'START1', 'START2', 'ADDRESS', 'LENGTH', 'DATA'])
     REAL_CHANNELS = 17
     MAX_CHANNELS = 24
+    CORRECTION = -50  # some difference between reading and writing
 
     def __init__(self):
         super().__init__('controller')
@@ -37,6 +38,7 @@ class Controller(Node):
         self.timer_period = 100 # in ms
         self.running = False
         self.msg_mutex = Lock()
+        self.hw_lock = Lock()
         self.declare_parameter('servo_config_file', 'servo_config.yaml')
         self.declare_parameter('pose_file', 'poses.yaml')
         self.declare_parameter('serial_device', '/dev/ttyAMA0')
@@ -65,39 +67,58 @@ class Controller(Node):
         self.joint_inc = [ 0 ] * self.MAX_CHANNELS
         self.goal = [ 0 ] * self.MAX_CHANNELS
         self.current_position = [ 0 ] * self.MAX_CHANNELS  # current postion
+        self.current_position[17] = 461
+        self.current_position[18] = 461
+        self.current_position[19] = 461
+        self.current_position[20] = 386
+        self.current_position[21] = 386
+        self.current_position[22] = 461
+        self.current_position[23] = 461
+        self.receive_count = 0
 
         # In 2 x 8bit hardware values
         self.hw_val = [ 0 ] * 2 * self.MAX_CHANNELS   # current message we are sending
 
         self.callback_group = ReentrantCallbackGroup()
+        self.timer_group = MutuallyExclusiveCallbackGroup()
+        self.status_group = MutuallyExclusiveCallbackGroup()
         self.pub_status = self.create_publisher(Status, '~/status', 10)
         self.sub = self.create_subscription(Joints, '~/cmd', self.cmd_callback, 10, callback_group=self.callback_group)
         self.srv_joints = self.create_service(JointControl, '~/control', self.control_callback, callback_group=self.callback_group)
 
     def compute_hwval(self):
-        for ch in range(self.MAX_CHANNELS):
-            self.hw_val[ch * 2] = int(self.current_position[ch] / 256)  # integer division
-            self.hw_val[ch * 2 + 1] = int(self.current_position[ch] % 256)
+        with self.hw_lock:
+            for ch in range(self.MAX_CHANNELS):
+                self.hw_val[ch * 2] = int(self.current_position[ch] / 256)  # integer division
+                self.hw_val[ch * 2 + 1] = int(self.current_position[ch] % 256)
+                #self.l.info('ch={} position={} hwval={} {}'.format(ch, self.current_position[ch], self.hw_val[ch * 2], self.hw_val[ch * 2 + 1]))
         #self.l.info('compute_hwval: {}'.format(self.hw_val))
 
     def set_goal(self, goal, dt, synchronous):
-        if synchronous:  # calculate step sizes
-            step_count = math.floor(dt / self.timer_period)
-            if step_count == 0:
-                self.l.warning('step_count=' + step_count)
-            delta = [goal[x] - self.current_position[x] for x in range(len(goal))]
-            self.joint_inc = [delta[x] / step_count for x in range(len(goal))]
-            self.goal_ticks = step_count
+        max_joint = -1
+        min_joint = -1
+        with self.hw_lock:
+            if synchronous:  # calculate step sizes
+                step_count = math.floor(dt / self.timer_period)
+                if step_count == 0:
+                    self.l.warning('step_count=' + step_count)
+                    return
+                delta = [goal[x] - self.current_position[x] for x in range(len(goal))]
+                self.joint_inc = [int(delta[x] // step_count) for x in range(len(goal))]
+                self.goal_ticks = step_count
 
-            max_delta = max(delta)
-            min_delta = min(delta)
-            #max_change = max(abs(max_delta), abs(min_delta)))
-            self.l.info('set_goal min_delta={} max_delta={} step_count={}'.format(
-                min_delta, max_delta, step_count))
-        else:
-            self.current_position = goal
-            self.compute_hwval()
-        self.goal = goal
+                max_delta = max(delta)
+                min_delta = min(delta)
+                #max_change = max(abs(max_delta), abs(min_delta)))
+                self.l.info('set_goal min_delta({})={} max_delta({})={} step_count={}'.format(
+                    delta.index(min_delta), min_delta,
+                    delta.index(max_delta), max_delta,
+                    step_count)
+                )
+            else:
+                self.current_position = goal
+                self.compute_hwval()
+            self.goal = goal
 
     def control_callback(self, control, response):
         self.l.info('Service request: ch={} enable={} home={}'.format(
@@ -112,7 +133,8 @@ class Controller(Node):
 
     def cmd_callback(self, msg):
         self.l.info('next pose: dt={}'.format(msg.dt))
-        self.set_goal(msg.position, msg.dt, msg.synchronous)
+        diff = [x - y for x, y in zip(self.stand, msg.position)]
+        self.set_goal(diff, msg.dt, msg.synchronous)
 
     def receiver(self):
         state = self.State.IDLE
@@ -123,7 +145,7 @@ class Controller(Node):
 
         self.running = True
         while self.running:
-            readbuf = self.ser.read(8)
+            readbuf = self.ser.read(1)
             #self.l.info("received {} data while in state={}".format(len(data), state))
             for b in readbuf:
                 if state == self.State.IDLE and b == 0xFF:
@@ -148,14 +170,18 @@ class Controller(Node):
                             self.l.warn('Bad CRC')
                             continue
                         if data[2] == 0xA3:
-                            position = data[3] * 256 + data[4]
+                            position = (data[3] * 256) + data[4] + self.CORRECTION
                             self.l.info('servo {}  position={}'.format(address, position))
-                            self.current_position[address] = position
+                            with self.hw_lock:
+                                self.current_position[address] = position
+                                if address == 16:
+                                    self.receive_count += 1
                             #self.l.info('Captured addr={0:02x} len={1:02x} data={2}'.format(address, data_len, data))
 
 
     def status_publisher(self):
         status = Status()
+        status.position = self.current_position
         self.pub_status.publish(status)
                         
     def send(self, data):
@@ -165,10 +191,11 @@ class Controller(Node):
         msg.append(crc)
         #self.l.info('sending: {}'.format(msg))
         self.ser.write(msg) 
+        self.ser.flush()
 
     def connect(self):
         self.l.info('Opening {}'.format(self.serial_device))
-        self.ser = serial.Serial(self.serial_device, 115200, timeout=1, rtscts=True)
+        self.ser = serial.Serial(self.serial_device, 115200, timeout=0.1, rtscts=True)
         self.receiver_thread = threading.Thread(target=self.receiver).start()
 
     def get_position(self):
@@ -186,8 +213,9 @@ class Controller(Node):
         channel = 0
         while channel < self.MAX_CHANNELS:
             test = [channel, 3, 2, 1]
+            self.l.info('enable channel {}'.format(channel))
             self.send(test)
-            time.sleep(0.002)
+            time.sleep(0.502)
             channel += 1
 
 
@@ -202,10 +230,18 @@ class Controller(Node):
             self.compute_hwval()
             self.goal_ticks -= 1
 
-        msg.extend(self.hw_val)
+        with self.hw_lock:
+            msg.extend(self.hw_val)
+
+        if False:
+            print('stand {}'.format(self.stand))
+            print('curr  {}'.format(self.current_position))
+            diff = [x - y for x, y in zip(self.stand, self.current_position)]
+            print('diff: {}'.format(diff))
         self.send(msg)
 
-    def start(self):
+    def start_up(self):
+        self.status_timer = self.create_timer(0.1, self.status_publisher, callback_group=self.status_group)
         while not self.running:
             time.sleep(0.5)
         self.send(self.initialize)
@@ -215,20 +251,25 @@ class Controller(Node):
         #self.enable_all()
         self.send(self.enable)
         time.sleep(0.002)
-        for x in range(10):
+        for x in range(4):
+            last_receive_count = self.receive_count
             self.get_position()
-        #self.compute_hwval()
-        self.set_goal(self.stand, 3000, True)
-        self.timer = self.create_timer(self.timer_period / 1000, self.controller)
+            while last_receive_count == self.receive_count:
+                self.l.warn('waiting')
+                time.sleep(0.001)
+            self.compute_hwval()
+        #self.set_goal(self.current_position, 1000, False)
+        #self.set_goal(self.stand, 1000, True)
+        self.timer = self.create_timer(self.timer_period / 1000, self.controller, callback_group=self.timer_group)
 
 
 def main(args=None):
     rclpy.init(args=args)
-    executor = rclpy.executors.MultiThreadedExecutor()
     controller = Controller()
+    executor = rclpy.executors.MultiThreadedExecutor()
     executor.add_node(controller)
     controller.connect()
-    controller.start()
+    controller.start_up()
 
     executor.spin()
     controller.running = False
