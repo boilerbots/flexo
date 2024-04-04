065aa125ab838cd1d746718f82f6f4a9c0a9d4f0
Curt Meyers <boilerbots@gmail.com>
2024-04-03 18:30:04 -0700

    A complete k1 description model
    It isn't great, there are mistakes like the servos rotate in the shoulder but it is a start.
    
    Signed-off-by: Curt Meyers <cmeyers@boilerbots.com>

---
+++ ros2/src/k1_description/launch/launch.py
@@ -0,0 +1,100 @@
+import os
+
+from ament_index_python.packages import get_package_share_directory
+
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.conditions import IfCondition
+from launch.substitutions import LaunchConfiguration
+from launch_ros.actions import Node
+from launch_ros.parameter_descriptions import ParameterValue
+from launch.substitutions import Command
+
+
+def generate_launch_description():
+    # Get the launch directory
+    package_dir = get_package_share_directory('k1_description')
+
+    # Launch configuration variables specific to simulation
+    rviz_config_file = LaunchConfiguration('rviz_config_file')
+    use_robot_state_pub = LaunchConfiguration('use_robot_state_pub')
+    use_joint_state_pub = LaunchConfiguration('use_joint_state_pub')
+    use_rviz = LaunchConfiguration('use_rviz')
+    urdf_file = LaunchConfiguration('urdf_file')
+    xacro_args = LaunchConfiguration('xacro_args')
+
+    declare_rviz_config_file_cmd = DeclareLaunchArgument(
+        'rviz_config_file',
+        default_value=os.path.join(package_dir, 'rviz', 'default.rviz'),
+        description='Full path to the RVIZ config file to use')
+    declare_use_robot_state_pub_cmd = DeclareLaunchArgument(
+        'use_robot_state_pub',
+        default_value='True',
+        description='Whether to start the robot state publisher')
+    declare_use_joint_state_pub_cmd = DeclareLaunchArgument(
+        'use_joint_state_pub',
+        default_value='True',
+        description='Whether to start the joint state publisher')
+    declare_use_rviz_cmd = DeclareLaunchArgument(
+        'use_rviz',
+        default_value='True',
+        description='Whether to start RVIZ')
+
+    declare_urdf_cmd = DeclareLaunchArgument(
+        'urdf_file',
+        default_value=os.path.join(package_dir, 'urdf', 'k1.xacro'),
+        description='Name of the used URDF file')
+
+    declare_xacro_cmd = DeclareLaunchArgument(
+        'xacro_args',
+        default_value="",
+        description='Arguments for xacro')
+
+    robot_description = ParameterValue(Command(['xacro ',
+                                                xacro_args,
+                                                " ",
+                                                urdf_file]),  # you can add your xacro arguments here
+                                       value_type=str)
+
+    start_robot_state_publisher_cmd = Node(
+        condition=IfCondition(use_robot_state_pub),
+        package='robot_state_publisher',
+        executable='robot_state_publisher',
+        name='robot_state_publisher',
+        output='screen',
+        # parameters=[{'use_sim_time': use_sim_time}],
+        parameters=[{
+            'robot_description': robot_description}])
+
+    start_joint_state_publisher_cmd = Node(
+        condition=IfCondition(use_joint_state_pub),
+        package='joint_state_publisher_gui',
+        executable='joint_state_publisher_gui',
+        name='joint_state_publisher_gui',
+        output='screen')
+
+    rviz_cmd = Node(
+        condition=IfCondition(use_rviz),
+        package='rviz2',
+        executable='rviz2',
+        name='rviz2',
+        arguments=['-d', rviz_config_file],
+        output='screen')
+
+    # Create the launch description and populate
+    ld = LaunchDescription()
+
+    # Declare the launch options
+    ld.add_action(declare_rviz_config_file_cmd)
+    ld.add_action(declare_urdf_cmd)
+    ld.add_action(declare_xacro_cmd)
+    ld.add_action(declare_use_robot_state_pub_cmd)
+    ld.add_action(declare_use_joint_state_pub_cmd)
+    ld.add_action(declare_use_rviz_cmd)
+
+    # Add any conditioned actions
+    ld.add_action(start_joint_state_publisher_cmd)
+    ld.add_action(start_robot_state_publisher_cmd)
+    ld.add_action(rviz_cmd)
+
+    return ld
