529021d27820d52fbdda3bde36c49e5279dfaf9b
Curt Meyers <boilerbots@gmail.com>
2024-04-06 20:05:49 -0700

    Seems to be a working moveIt with controller
    Signed-off-by: Curt Meyers <cmeyers@boilerbots.com>

---
+++ ros2/src/k1_control/src/k1_hardware.cpp
@@ -0,0 +1,133 @@
+
+#include "rclcpp/rclcpp.hpp"
+#include "k1_control/k1_hardware.h"
+#include <string>
+#include <vector>
+#include "k1_interfaces/msg/Joints.hpp"
+
+namespace k1_control
+{
+CallbackReturn RobotSystem::on_init(const hardware_interface::HardwareInfo & info)
+{
+  rclcpp::Logger logger = rclcpp::get_logger("k1_hardware");
+
+  if (hardware_interface::SystemInterface::on_init(info) != CallbackReturn::SUCCESS)
+  {
+    return CallbackReturn::ERROR;
+  }
+
+  // robot has 17 joints
+  joint_position_.assign(17, 0);
+  joint_velocities_.assign(17, 0);
+  joint_position_command_.assign(17, 0);
+  joint_velocities_command_.assign(17, 0);
+
+  // force sensor has 6 readings
+  ft_states_.assign(6, 0);
+  ft_command_.assign(6, 0);
+
+  for (const auto & joint : info_.joints)
+  {
+    for (const auto & interface : joint.state_interfaces)
+    {
+      RCLCPP_INFO(logger, "Interface: %s  Joint %s", interface.name.c_str(), joint.name.c_str());
+      joint_interfaces[interface.name].push_back(joint.name);
+    }
+  }
+
+  return CallbackReturn::SUCCESS;
+}
+
+std::vector<hardware_interface::StateInterface> RobotSystem::export_state_interfaces()
+{
+  std::vector<hardware_interface::StateInterface> state_interfaces;
+
+  int ind = 0;
+  for (const auto & joint_name : joint_interfaces["position"])
+  {
+    state_interfaces.emplace_back(joint_name, "position", &joint_position_[ind++]);
+  }
+
+  ind = 0;
+  for (const auto & joint_name : joint_interfaces["velocity"])
+  {
+    state_interfaces.emplace_back(joint_name, "velocity", &joint_velocities_[ind++]);
+  }
+
+  state_interfaces.emplace_back("tcp_fts_sensor", "force.x", &ft_states_[0]);
+  state_interfaces.emplace_back("tcp_fts_sensor", "force.y", &ft_states_[1]);
+  state_interfaces.emplace_back("tcp_fts_sensor", "force.z", &ft_states_[2]);
+  state_interfaces.emplace_back("tcp_fts_sensor", "torque.x", &ft_states_[3]);
+  state_interfaces.emplace_back("tcp_fts_sensor", "torque.y", &ft_states_[4]);
+  state_interfaces.emplace_back("tcp_fts_sensor", "torque.z", &ft_states_[5]);
+
+  return state_interfaces;
+}
+
+std::vector<hardware_interface::CommandInterface> RobotSystem::export_command_interfaces()
+{
+  std::vector<hardware_interface::CommandInterface> command_interfaces;
+
+  int ind = 0;
+  for (const auto & joint_name : joint_interfaces["position"])
+  {
+    command_interfaces.emplace_back(joint_name, "position", &joint_position_command_[ind++]);
+  }
+
+  ind = 0;
+  for (const auto & joint_name : joint_interfaces["velocity"])
+  {
+    command_interfaces.emplace_back(joint_name, "velocity", &joint_velocities_command_[ind++]);
+  }
+
+  command_interfaces.emplace_back("tcp_fts_sensor", "force.x", &ft_command_[0]);
+  command_interfaces.emplace_back("tcp_fts_sensor", "force.y", &ft_command_[1]);
+  command_interfaces.emplace_back("tcp_fts_sensor", "force.z", &ft_command_[2]);
+  command_interfaces.emplace_back("tcp_fts_sensor", "torque.x", &ft_command_[3]);
+  command_interfaces.emplace_back("tcp_fts_sensor", "torque.y", &ft_command_[4]);
+  command_interfaces.emplace_back("tcp_fts_sensor", "torque.z", &ft_command_[5]);
+
+  return command_interfaces;
+}
+
+return_type RobotSystem::read(const rclcpp::Time & /*time*/, const rclcpp::Duration & period)
+{
+#if 0
+  rclcpp::Logger logger = rclcpp::get_logger("k1_hardware");
+
+  for (auto i = 0ul; i < joint_velocities_command_.size(); i++)
+  {
+    joint_velocities_[i] = joint_velocities_command_[i];
+    joint_position_[i] += joint_velocities_command_[i] * period.seconds();
+  }
+#endif
+
+  for (auto i = 0ul; i < joint_position_command_.size(); i++)
+  {
+    //RCLCPP_INFO(logger, "%d position %lf", i, joint_position_command_[i]);
+    joint_position_[i] = joint_position_command_[i];
+    //joint_position_[i] = i * 0.1;
+  }
+
+  return return_type::OK;
+}
+
+return_type RobotSystem::write(const rclcpp::Time &, const rclcpp::Duration &)
+{
+#if 0
+  rclcpp::Logger logger = rclcpp::get_logger("k1_hardware");
+
+  for (auto i = 0ul; i < joint_position_command_.size(); i++)
+  {
+    RCLCPP_INFO(logger, "position %lf", joint_position_command_[i]);
+  }
+#endif
+
+  return return_type::OK;
+}
+
+}
+
+#include "pluginlib/class_list_macros.hpp"
+
+PLUGINLIB_EXPORT_CLASS(k1_control::RobotSystem, hardware_interface::SystemInterface)
