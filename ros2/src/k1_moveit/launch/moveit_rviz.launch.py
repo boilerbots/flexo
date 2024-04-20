8c1d420852bcc438deb0d1d02c660e9dcdc67650
Curt Meyers <boilerbots@gmail.com>
2024-04-19 23:06:46 -0700

    Adding sqlite database to moveit_rviz.launch
    Signed-off-by: Curt Meyers <cmeyers@boilerbots.com>

--- ros2/src/k1_moveit/launch/moveit_rviz.launch.py
+++ ros2/src/k1_moveit/launch/moveit_rviz.launch.py
@@ -1,5 +1,41 @@
 from moveit_configs_utils import MoveItConfigsBuilder
-from moveit_configs_utils.launches import generate_moveit_rviz_launch
+#  from moveit_configs_utils.launches import generate_moveit_rviz_launch
+
+
+def generate_moveit_rviz_launch(moveit_config):
+    """Launch file for rviz"""
+    ld = LaunchDescription()
+
+    ld.add_action(DeclareBooleanLaunchArg("debug", default_value=False))
+    ld.add_action(
+        DeclareLaunchArgument(
+            "rviz_config",
+            default_value=str(moveit_config.package_path / "config/moveit.rviz"),
+        )
+    )
+
+    warehouse_ros_config = {
+        "warehouse_plugin": "warehouse_ros_sqlite::DatabaseConnection",
+        "warehouse_host": "/home/cmeyers/projects/flexo/warehouse_db.sqlite",
+    }
+
+    rviz_parameters = [
+        moveit_config.planning_pipelines,
+        moveit_config.robot_description_kinematics,
+        warehouse_ros_config,
+    ]
+
+    add_debuggable_node(
+        ld,
+        package="rviz2",
+        executable="rviz2",
+        output="log",
+        respawn=False,
+        arguments=["-d", LaunchConfiguration("rviz_config")],
+        parameters=rviz_parameters,
+    )
+
+    return ld
 
 
 def generate_launch_description():
